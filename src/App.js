c87374bf2f8b1f1867e1423f6498747c057d2141
Bita Fotovvat <bita.ftvt@gmail.com>
2024-05-13 12:58:31 -0400

    changes made

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import './App.scss';
+import QRCode from 'qrcode';
+import { useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode ] = useState('');
+
+  const GenerateQRCode = (e) => {
+    e.preventdefault();
+    QRCode.toDataURL(url, (err, url) => {
+      if(!url){
+        alert('Please enter a valid url.')
+        return;
+      }
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(url);
+      setQrcode(url);
+    })
+  }
+
+
   return (
     <div className="App">
       <h1 className='qr__title'>QR Code Generator</h1>
       <form className='qr__form'>
-        <input
-        className='qr__input' 
-        type="text" 
-        placeholder='Name'
+        {/* <input
+          className='qr__input' 
+          type="text" 
+          placeholder='Name'
         />
         <input 
-        className='qr__input'
-        type="text" 
-        placeholder='Phone Number'
+          className='qr__input'
+          type="text" 
+          placeholder='Phone Number'
         />
         <input 
-        className='qr__input'
-        type="text" 
-        placeholder='email'
-        />
+          className='qr__input'
+          type="text" 
+          placeholder='email'
+        /> */}
         <input 
-        className='qr__input'
-        type="text" 
-        placeholder='Website'
+          className='qr__input'
+          type="text" 
+          placeholder='Website'
+          value={url}
+          onChange={e=> setUrl(e.target.value)}
         />
-        <button className='qr__button'>Generate</button>
+        <button className='qr__button' onClick={GenerateQRCode}>Generate</button>
       </form>
+      <img src={qrcode} alt="QR Code" />
     </div>
   );
 }
-
 export default App;
